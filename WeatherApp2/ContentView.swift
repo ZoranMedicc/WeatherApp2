9bb37cb985df655ee555d3ccbb5c6ef234dca00b
Zoran Medic <zomedic@hotmail.com>
2023-01-10 20:47:01 +0100

    added hourly and 10 day basic view

--- WeatherApp2/ContentView.swift
+++ WeatherApp2/ContentView.swift
@@ -91,9 +91,24 @@ struct TenDaysForecastView: View {
             List(dayWeatherList, id: \.date) { dailyWeather in
                 HStack {
                     Text(dailyWeather.date.formatAsAbbreviatedDay())
-                }
+                        .frame(maxWidth: 50, alignment: .leading)
+                    Image(systemName: "\(dailyWeather.symbolName).fill")
+                        .foregroundColor(.yellow)
+                    
+                    Text(dailyWeather.lowTemperature.formatted())
+                        .frame(maxWidth: .infinity)
+                    
+                    Text(dailyWeather.highTemperature.formatted())
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }.listRowBackground(Color.blue)
             }.listStyle(.plain)
         }
+        .padding()
+        .background(content: {
+            Color.blue
+        })
+        .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
+        .foregroundColor(.white)
     }
 }
 
